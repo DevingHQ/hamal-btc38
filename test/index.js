0f00a8cef43300023e8023bc8bf549522ff29f1f
Jackie Meng <mengjq08@gmail.com>
2017-05-09 21:37:17 +0800

    delete key

--- test/index.js
+++ test/index.js
@@ -2,9 +2,9 @@
 import Btc38 from '../lib';
 
 const btc = new Btc38({
-  userId: 206992,
-  key: '0f8d972e5eb95f76ad5f583cd09e5e44',
-  secret: 'd28fb0894f7ee9ea8acb39cce3f99bcf9029a21259aa22dd615be15885d2b597'
+  userId: '',
+  key: '',
+  secret: ''
 });
 
 describe('hamal-btc-38', function () {
